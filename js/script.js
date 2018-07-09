b04c5debfecaf64f69b62e3130f79ec30a52d24e
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-07-09 16:02:26 +0300

    add hambuger menu oh html, style on css and functionality on jscript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    alert("im good");
+  });
+});
 
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
